d8feb922a0a36fff178de48600286dc8cc6c6c3b
estevansouza <estevancrimadesouza@gmail.com>
2025-12-04 11:17:23 +0000

    adicionando códio de concatenação

---
+++ concat-dados.py
@@ -0,0 +1,7 @@
+# Vamos receber dois dados diferentes do usuário e concatená-los em uma única String.
+info1 = input("Digite a primeira informação: ")
+info2 = input("Digite a segunda informação: ")
+
+info_concatenada = info1 + " " + info2
+
+print("Informação concatenada:", info_concatenada)
