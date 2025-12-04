0f6181420d5bce15b27e77adde7b98a889e6d0f4
estevansouza <estevancrimadesouza@gmail.com>
2025-12-04 11:35:03 +0000

    correção do print

--- repet-textos.py
+++ repet-textos.py
@@ -1,5 +1,4 @@
 #Vamos receber um input de string e um input numérico do usuário e repetir a string o número de vezes especificado.
 texto = input("Digite um texto: ")
 vezes = int(input("Digite o número de vezes que deseja repetir o texto: "))
-texto_repetido = texto * vezes
-print("Texto repetido:\n", texto_repetido)
+print("Texto repetido: ",( texto + " ") * vezes)
